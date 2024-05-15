a3db8293909adf95e3015daaa41cb9a8182f4791
Indrawan <indra@INDRAWANWIJAYA>
2024-05-15 17:37:52 +0700

    Update Header, Footer

---
+++ SANTARA Marketplace/Template/HeaderAndFooter.Master.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace SANTARA_Marketplace.Template
+{
+    public partial class HeaderAndFooter : System.Web.UI.MasterPage
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
