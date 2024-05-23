0354e4b9b6d69e355d23cd7a4db136929ddc01fd
IndrawanWijaya <IndrawanWijaya@DESKTOP-KO121GM>
2024-05-23 15:19:01 +0700

    Add MasterPage

--- SANTARA Marketplace/Template/HeaderAndFooter.Master.cs
+++ SANTARA Marketplace/Template/HeaderAndFooter.Master.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Globalization;
 using System.Linq;
 using System.Web;
 using System.Web.UI;
@@ -12,6 +13,35 @@ namespace SANTARA_Marketplace.Template
         protected void Page_Load(object sender, EventArgs e)
         {
 
+        }
+
+        public static string GetImageBase64String(object imageData)
+        {
+            if (imageData == null)
+            {
+                return string.Empty;
+            }
+
+            if (imageData is byte[] data)
+            {
+                //string mimeType = IsPngImage(data) ? "image/png" : "image/jpeg";
+                string mimeType = "image/png";
+                string base64String = Convert.ToBase64String(data);
+                return $"data:{mimeType};base64,{base64String}";
+            }
+
+            return string.Empty;
+
+        }
+
+        public static String GetPrice(object ProductPrice)
+        {
+            int price = (int)ProductPrice;
+
+            NumberFormatInfo nfi = new CultureInfo("id-ID").NumberFormat;
+            nfi.CurrencyDecimalDigits = 0;
+
+            return price.ToString("C", nfi);
         }
     }
 }
